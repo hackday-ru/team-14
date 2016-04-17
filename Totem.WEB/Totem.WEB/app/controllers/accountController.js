65b385861c9f98b6bb093a4b2c940ebcd1551c7b
dislack <boikovdmitriy@mail.ru>
2016-04-17 08:34:26 +0300

    added dialogs

--- Totem.WEB/Totem.WEB/app/controllers/accountController.js
+++ Totem.WEB/Totem.WEB/app/controllers/accountController.js
@@ -1,40 +1,64 @@
 ﻿'use strict';
-app.controller('accountController', ['$scope', '$location', 'accountService', 'ngDialog', function ($scope, $location, accountService, ngDialog) {
+app.controller('accountController', [
+    '$scope', '$location', 'accountService', 'ngDialog', function($scope, $location, accountService, ngDialog) {
 
-    $scope.userInformation = {
-        userName: "",
-        information: "",
-        imagePath: "",
-        balance: ""
-    };
+        $scope.userInformation = {
+            userName: "",
+            information: "",
+            imagePath: "",
+            balance: ""
+        };
+        var users =
+        [
+            { name: "Андрей", description: "1234@gmail.com", money: "1024" },
+            { name: "Антон", description: "2345@gmail.com", money: "3.4" },
+            { name: "Дмитрий", description: "3456@gmail.com", money: "359" },
+            { name: "Твоя мама", description: "milf@g.com", money: "69" }
+        ];
 
-    $scope.message = "";
+        $scope.currentUser = users[0];
+        $scope.message = "";
+        $scope.task = {
+            name: "",
+            description: "",
+            countMoney: ""
+        };
 
-  
-var users = 
-    [
-        { name: "Андрей",  description: "1234@gmail.com", money: "1024"},
-        { name: "Антон", description: "2345@gmail.com", money: "3.4" },
-        { name: "Дмитрий", description: "3456@gmail.com", money: "359" },
-        { name: "Твоя мама", description: "milf@g.com", money: "69" }
-    ]
-;
-  
+
         $scope.listUsers = users;
-       // $scope.addItem = function (userName, information, balance) {
-      
-   // };
-    
+
 
         var tasks =
         [
             { name: "task1", description: "eat keepchuk", countMoney: "100500" },
             { name: "task2", description: "drink vodka", countMoney: "6000000" },
-            { name: "task3", description: "make sense", countMoney: "123456" },
-       ]
-    ;
-       $scope.listTasks = tasks;
- 
+            { name: "task3", description: "make sense", countMoney: "123456" }
+        ];
+        $scope.listTasks = tasks;
+        $scope.addMoney= function(money) {
+            $scope.currentUser.money += money;
+            ngDialog.close();
+        }
 
+        $scope.addMoneyDialog = function (money) {
+            ngDialog.open({
+                template: 'app/views/shared/_addMoney.html',
+                className: 'ngdialog-theme-default',
+                scope: $scope
+            });
+        }
 
-}]);
+        $scope.addTask = function () {
+            $scope.listTasks.push($scope.task);
+            ngDialog.close();
+        }
+
+        $scope.addTaskDialog = function (money) {
+            ngDialog.open({
+                template: 'app/views/shared/_addTask.html',
+                className: 'ngdialog-theme-default',
+                scope: $scope
+            });
+        }
+    }
+]);
